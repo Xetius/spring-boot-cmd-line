81a96be83db4ad3c2aa0cd58ff5bf0162cd51d97
Chris Hudson <chris@xetius.com>
2014-03-07 18:06:24 +0000

    Initial push

---
+++ src/main/java/com/xetius/Application.java
@@ -0,0 +1,22 @@
+package com.xetius;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Something something");
+    }
+}
